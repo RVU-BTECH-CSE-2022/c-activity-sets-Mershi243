9a9e2a8f1ea6c2086e9c9dbe3b580df887f5a9dc
Mershika Uthaya  Uthaya <115769794+Mershi243@users.noreply.github.com>
2022-12-09 04:31:52 +0000

    problem done

--- set01/problem10.c
+++ set01/problem10.c
@@ -1,19 +1,71 @@
-#include<stdio.h>
+#include <stdio.h>
+
+void input_two_strings(char *string1, char *string2);
+int stringcompare(char *string1, char *string2);
+void output(char *string1, char *string2, int result);
+
 int main()
 {
-  char str1[20],str[25];
-  printf("enter the two word:");
-  scanf(%s%s,&str1,&str2);
+   char string1[100], string2[100];
+   int result=0;
+   input_two_strings(string1, string2);
+    result=stringcompare(string1, string2);
+    output(string1, string2, result);
+}
+void input_two_strings(char *string1, char *string2)
+{
+    printf("Enter the first string\n");
+    scanf("%s", string1);
+   printf("Enter the second string\n");
+   scanf("%s", string2);
+}
 
-  int i=0;
-  while(str1[i]='/0' &&str2[1]='\0')
-    {
-      if(str1[i]=str2[i])
-    }
+int stringcompare(char *string1, char *string2)
+{
+   int result=0, i=0, flag=0;
+   for(i=0; string1[i]!='\0' && string2[i]!='\0'; i++)
+     {
 
+         if(string1[i]!=string2[i])
+         {
+            flag=1;
+            break;
+         }
+     }
 
+if(flag==0)
+{
+    if(string1[i]=='\0' && string2[i]=='\0')
+      return 0;
+  if(string1[i]=='\0')
+    return 1;
+  if(string2[i]=='\0')
+    return 2;
+  }
 
+if(flag==1)
+{
+     if(string1[i]>string2[i])
+       return 1;
+  else return 2;
+}
+  }
 
+void output(char *string1, char *string2, int result)
+{
+  if(result==0)
+    printf("The strings are  equal\n");
+  if(result==1)
+    printf("%s is greater\n", string1);
+  if(result==2)
+     printf("%s is greater\n", string2);
+  
+}
+  
+
+  
+  
+      
 
 
 
